f6cbac5c6ab9bd9d0adc098cad6f1fda4558445c
Tim <tim.ho@duke.edu>
2023-02-25 17:58:25 -0500

    implemented aliases, callback for button0

--- src/main.c
+++ src/main.c
@@ -6,43 +6,64 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
 
+LOG_MODULE_REGISTER(Zephyr_Devicetree_GPIO_Callbacks,LOG_LEVEL_DBG);
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
-
-/* The devicetree node identifier for the "led0" alias. */ 
 
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
 static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(DT_ALIAS(heartbeat), gpios);
-static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(DT_ALIAS(error), gpios);
+static const struct gpio_dt_spec led4 = GPIO_DT_SPEC_GET(DT_ALIAS(error), gpios);
 static const struct gpio_dt_spec button0 = GPIO_DT_SPEC_GET(DT_ALIAS(button0), gpios);
+
+static struct gpio_callback button0_cb;
+
+static int button0_event = 0;
+
+void button0_callback(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+button0_event = 1; // conditional statement in main() can now do something based on the event detection
+}
 
 void main(void)
 {
-	int ret;
+  int err;
 
-	if (!device_is_ready(led0.port)) {
-		return;
+  err = !device_is_ready(led4.port);
+	if (err) {
+		LOG_ERR("gpio1 interface not ready.");
+    return;
 	}
 
-  if (!device_is_ready(button0.port)) {
+  err = !device_is_ready(button0.port);
+  if (err) {
     LOG_ERR("gpio0 interface not ready.");
-    return -1;
+    return;
   }
 
-	ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		return;
+	err = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE | GPIO_ACTIVE_LOW);
+	if (err < 0) {
+		LOG_ERR("Cannot configure sleep button");
+    return;
 	}
 
+  err = gpio_pin_interrupt_configure_dt(&button0, GPIO_INT_EDGE_TO_ACTIVE);
+  if (err < 0) {
+    LOG_ERR("Cannot attach callback to button0.");
+  }
+  gpio_init_callback(&button0_cb, button0_callback, BIT(button0.pin));
+  gpio_add_callback(button0.port, &button0_cb);
+
 	while (1) {
-		ret = gpio_pin_toggle_dt(&led0);
-		if (ret < 0) {
-			return;
-		}
+    if (button0_event) {
+    // Do something
+      //err = gpio_pin_configure_dt(&led0, GPIO_ACTIVE_LOW);
+      button0_event = 0;
+    }
 		k_msleep(SLEEP_TIME_MS);
 	}
 }
