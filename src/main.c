dfd7475c371ad2380d2398d736546b87c19bc828
Tim <tim.ho@duke.edu>
2023-02-25 16:27:50 -0500

    fixed _ complication bug

--- src/main.c
+++ src/main.c
@@ -10,30 +10,36 @@
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
+/* The devicetree node identifier for the "led0" alias. */ 
 
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(DT_ALIAS(heartbeat), gpios);
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(DT_ALIAS(error), gpios);
+static const struct gpio_dt_spec button0 = GPIO_DT_SPEC_GET(DT_ALIAS(button0), gpios);
 
 void main(void)
 {
 	int ret;
 
-	if (!device_is_ready(led.port)) {
+	if (!device_is_ready(led0.port)) {
 		return;
 	}
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+  if (!device_is_ready(button0.port)) {
+    LOG_ERR("gpio0 interface not ready.");
+    return -1;
+  }
+
+	ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0) {
 		return;
 	}
 
 	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
+		ret = gpio_pin_toggle_dt(&led0);
 		if (ret < 0) {
 			return;
 		}
